42983c5282b2d45627e15be3b4351145d5f29be5
Your Name <you@example.com>
2023-05-06 07:24:13 +0530

    Homepage three sections added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,13 +19,31 @@ import {
   Heading,
   Divider,
   ButtonGroup,
+  VStack,
+  // Image,
 } from "@chakra-ui/react";
 
 import style from "./Navbar.module.css";
 
 import { SearchIcon } from "@chakra-ui/icons";
+import { async } from "q";
+import { useEffect, useState } from "react";
+import { left } from "@popperjs/core";
 
 export default function Navbar() {
+  const [topCategoriesData, settopCategoriesSection] = useState([]);
+
+  const topCategoriesSection = async () => {
+    const response = await fetch("http://localhost:8080/topCatData");
+    const data = await response.json();
+    settopCategoriesSection(data);
+    return data.meals;
+  };
+
+  useEffect(() => {
+    topCategoriesSection();
+  }, []);
+
   return (
     <Box className={style.mainContainer}>
       <Box className={style.upperPortion}>
@@ -187,6 +205,129 @@ export default function Navbar() {
             </CardBody>
           </Card>
         </SimpleGrid>
+      </Box>
+      <Box className={style.topCategoriesSection}>
+        <Text className={style.text3} textAlign={"left"}>
+          Top
+          <Text>Categories</Text>
+        </Text>
+        <SimpleGrid columns={4} gap={4}>
+          {topCategoriesData.map((item) => {
+            return (
+              <Card padding={10}>
+                <VStack>
+                  <Image src={item.image} style={{ width: "50px" }} />
+                  <Text
+                    style={{
+                      color: "#765AB1FF",
+                      fontSize: "16px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    {item.title}
+                  </Text>
+                </VStack>
+              </Card>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
+      <Box className={style.midVideoPart}>
+        <SimpleGrid columns={2}>
+          <Image src="https://app.visily.ai/projects/b9c1d17e-9922-4dc8-be73-5b0359ab46a3/boards/473708/elements/161325609" />
+          <Box style={{ backgroundColor: "#765AB1FF" }}>
+            <Box style={{ padding: "155px", textAlign: "left" }}>
+              <Text className={style.text1}>SUCCESS</Text>
+              <br />
+              <br />
+              <Text style={{ fontSize: "42px", lineBreak: "auto" }}>
+                Photos & Lights <br />
+                Essentials
+              </Text>
+              <br />
+              {/* <br /> */}
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur <br />
+                adipisicing elit,sed do eiusmod tempor Best <br /> incididunt ut
+                labore et aliqua.
+              </Text>
+              <br />
+              <br />
+              <Button
+                height="64px"
+                width="180px"
+                bg={"#1AC843FF"}
+                padding={"30px"}
+                fontSize={"14px"}
+                borderRadius={"0px"}
+              >
+                LEARN MORE
+              </Button>
+            </Box>
+          </Box>
+        </SimpleGrid>
+      </Box>
+      <Box className={style.newCourseSection} marginTop={100}>
+        <Text className={style.text3} textAlign={"left"}>
+          New <br /> Courses
+        </Text>
+        <SimpleGrid columns={3}>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://img.freepik.com/premium-vector/set-mockups-smartphones-with-user-interface-mobile-application_451939-184.jpg?w=740"
+                // alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">UI & UX Design Training</Heading>
+                <Text>Bill Weinman</Text>
+                <Text color="blue.600" fontSize="2xl">
+                  2h 11min
+                </Text>
+                <Text>1,489,310 View</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://img.freepik.com/premium-vector/set-mockups-smartphones-with-user-interface-mobile-application_451939-184.jpg?w=740"
+                // alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">UI & UX Design Training</Heading>
+                <Text>Bill Weinman</Text>
+                <Text color="blue.600" fontSize="2xl">
+                  2h 11min
+                </Text>
+                <Text>1,489,310 View</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+          <Card maxW="sm">
+            <CardBody>
+              <Image
+                src="https://img.freepik.com/premium-vector/set-mockups-smartphones-with-user-interface-mobile-application_451939-184.jpg?w=740"
+                // alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">UI & UX Design Training</Heading>
+                <Text>Bill Weinman</Text>
+                <Text color="blue.600" fontSize="2xl">
+                  2h 11min
+                </Text>
+                <Text>1,489,310 View</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        </SimpleGrid>
+      </Box>
+      <Box className={style.footer}>
+        <Box className={style.upperFooter}></Box>
+        <Box className={style.lowerFooter}></Box>
       </Box>
     </Box>
   );
